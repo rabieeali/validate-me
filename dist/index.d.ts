aaa36579e20f336eececd2d1352f303fb421087c
rabieeali <alirbi.webdev@gmail.com>
2023-05-30 12:20:51 +0330

    changed name to validate-me

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,25 +1,4 @@
-declare module '@alirabiee/validate-me' {
-    /**
-     * Checks if the input string is a valid email address.
-     * @param email The email address to validate.
-     * @returns `true` if the email is valid, `false` otherwise.
-     */
-    export function isEmail(email: string): boolean;
-  
-    /**
-     * Checks if the input string has a length within the specified range.
-     * @param input The input string to check the length of.
-     * @param minLength The minimum length allowed.
-     * @param maxLength The maximum length allowed.
-     * @returns `true` if the input length is within the specified range, `false` otherwise.
-     */
-    export function hasLength(input: string, minLength: number, maxLength: number): boolean;
-  
-    /**
-     * Checks if the input string has a length of zero.
-     * @param input The input string to check.
-     * @returns `true` if the input length is zero, `false` otherwise.
-     */
-    export function isEmpty(input: string): boolean;
-  }
-  
+declare function isEmail(email: string): boolean;
+declare function hasLength(input: string, minLength: number, maxLength: number): boolean;
+declare function isEmpty(input: string): boolean;
+export { hasLength, isEmail, isEmpty };
--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isEmpty = exports.isEmail = exports.hasLength = void 0;
 function isEmail(email) {
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
 exports.isEmail = isEmail;
 function hasLength(input, minLength, maxLength) {
-    var inputLength = input.length;
+    const inputLength = input.length;
     return inputLength >= minLength && inputLength <= maxLength;
 }
 exports.hasLength = hasLength;
