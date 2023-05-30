19f751c6f26ebdf075fa45ee5e411bb8e5f03fcd
rabieeali <alirbi.webdev@gmail.com>
2023-05-30 13:03:10 +0330

    added hasAllItems and  persian phone number validation

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isEmpty = exports.isEmail = exports.hasLength = void 0;
+exports.isEmpty = exports.isEmail = exports.hasLength = exports.hasAllItems = exports.isPersianPhoneNumber = void 0;
 function isEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
 exports.isEmail = isEmail;
 function hasLength(input, minLength, maxLength) {
-    const inputLength = input.length;
+    var inputLength = input.length;
     return inputLength >= minLength && inputLength <= maxLength;
 }
 exports.hasLength = hasLength;
@@ -15,3 +15,18 @@ function isEmpty(input) {
     return input.length === 0;
 }
 exports.isEmpty = isEmpty;
+function hasAllItems(input, items) {
+    for (var _i = 0, items_1 = items; _i < items_1.length; _i++) {
+        var item = items_1[_i];
+        if (input.indexOf(item) === -1) {
+            return false;
+        }
+    }
+    return true;
+}
+exports.hasAllItems = hasAllItems;
+function isPersianPhoneNumber(input) {
+    var regex = /(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/;
+    return regex.test(input);
+}
+exports.isPersianPhoneNumber = isPersianPhoneNumber;
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,24 @@ function isEmail(email: string): boolean {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
-
 function hasLength(input: string, minLength: number, maxLength: number): boolean {
     const inputLength = input.length;
     return inputLength >= minLength && inputLength <= maxLength;
 }
-
-
 function isEmpty(input: string): boolean {
     return input.length === 0;
 }
+function hasAllItems(input: string, items: string[]): boolean {
+    for (const item of items) {
+        if (input.indexOf(item) === -1) {
+            return false;
+        }
+    }
+    return true;
+}
+function isPersianPhoneNumber(input: string) {
+    const regex = /(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/;
+    return regex.test(input);
+}
 
-export { hasLength, isEmail, isEmpty }
+export { isPersianPhoneNumber, hasAllItems, hasLength, isEmail, isEmpty }
